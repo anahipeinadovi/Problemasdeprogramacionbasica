ab2cd673c79913ae20fc7d3c80e2368118766658
anahipeinadovi <a353262@uach.mx>
2023-04-04 19:56:49 -0600

    se agrega el problema 3

---
+++ prob3.js
@@ -0,0 +1,10 @@
+//Problema 3 
+// Hacer un programa que imprima los números pares entre 0 y 100. 
+
+let number = 0;
+
+while(number<=100){
+    if(number%2 == 0)
+        console.log(number);
+        number++;
+}
