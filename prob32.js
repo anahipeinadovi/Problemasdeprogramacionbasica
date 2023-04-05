1fe99f8896cab152eaa06a31e6f9b73eae389c1c
anahipeinadovi <a353262@uach.mx>
2023-04-04 20:00:50 -0600

    se agrega el problema 32

---
+++ prob32.js
@@ -0,0 +1,40 @@
+//32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista
+//debiendo ir cada uno en una fila distinta. 
+
+
+const personas = [];
+
+personas.push("Juan");
+personas.push("María");
+personas.push("Anahi");
+personas.push("Luis");
+personas.push("Ana");
+personas.push("Diego");
+personas.push("Marta");
+personas.push("Sofía");
+personas.push("Tomás");
+personas.push("Lucía");
+personas.push("Fernando");
+personas.push("Pablo");
+personas.push("Carolina");
+personas.push("Andrés");
+personas.push("Lucas");
+personas.push("Roxana");
+personas.push("Javier");
+personas.push("Isabel");
+personas.push("Camila");
+personas.push("Ricardo");
+
+
+for (let i = 0; i < personas.length; i++) {
+  console.log(personas[i]);
+}
+
+
+
+
+
+
+
+
+
