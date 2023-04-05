6111ff6ab56df1d97bb727b25ee11687da1eac85
anahipeinadovi <a353262@uach.mx>
2023-04-04 19:56:58 -0600

    se agrega el problema 4

---
+++ prob4.js
@@ -0,0 +1,12 @@
+//4.- Hacer un programa que imprima la suma de los 100 primeros números. 
+
+let number = 0;
+let suma = 0;
+
+while(number<=100){
+    suma+=number;
+    number++;
+    
+}
+console.log(suma);
+
