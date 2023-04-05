7300cc9d1c64c8b293fb3832126343ef6b072793
anahipeinadovi <a353262@uach.mx>
2023-04-04 19:58:40 -0600

    se agrega el problema 16

---
+++ prob16.js
@@ -0,0 +1,10 @@
+// 16.- Imprimir diez veces la serie de números del 1 al 10. 
+
+k=1;
+while(k<=10){
+    console.log(""+"=============="+"serie"+k+"=================");
+    for(i=1;i<=10;i++){
+        console.log(i);
+    }
+    k++;
+}
