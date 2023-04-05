a22597f355342a96c974b3edf0d6ee1d0df8b6ac
anahipeinadovi <a353262@uach.mx>
2023-04-04 19:56:05 -0600

    se agrega el problema 1

---
+++ prob1.js
@@ -0,0 +1,22 @@
+//Problema 1 
+
+//1.- Hacer un programa que imprima los números del 1 al 100
+
+
+
+let number=0;
+
+while(number<=100){
+    console.log(number);
+    number++
+    
+}
+
+
+
+
+
+
+
+
+
